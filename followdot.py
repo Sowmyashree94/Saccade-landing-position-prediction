6c21925140bceeb7cd4ae852dedd648035e88680
Jagan <jaganshanmugam@outlook.com>
2018-12-09 18:00:26 +0100

    Follow the dot

--- followdot.py
+++ followdot.py
@@ -4,15 +4,15 @@ import random
 
 pygame.init()
 offset = 50
-screen_resolution = (1980, 1080)
+screen_resolution = (1920, 1080)
 mid_x = int(screen_resolution[0]/2)
 mid_y = int(screen_resolution[1]/2)
 max_x = screen_resolution[0] - offset
 max_y = screen_resolution[1] - offset
  
 
-screen = pygame.display.set_mode(screen_resolution)
-pygame.display.toggle_fullscreen()
+screen = pygame.display.set_mode(screen_resolution, pygame.FULLSCREEN)
+#pygame.display.toggle_fullscreen()
 screen_color = (255,255,255)
 dot_color = (255,0,0)
 radius = 15
@@ -20,13 +20,16 @@ thickness = 15
 
 points = [(offset,offset), (max_x,max_y), (max_x, offset), (offset, max_y), (mid_x, mid_y), (offset, mid_y), (mid_x, offset), (mid_x, max_y), (max_x, mid_y)]
 print(points)
+#for mode in pygame.display.list_modes():
+#	print(mode)
 i = 0
 while (1):
+	
 	try:    
 		screen.fill(screen_color)
 		x,y = random.choice(points)
 		#x, y = points[i]		
-		print (x,y)		
+		#print (x,y)		
 		pygame.draw.circle(screen, dot_color, (x, y), radius, thickness)
 		pygame.display.update()
 		pygame.time.delay(3000)
