1518a850861c205173bbc174637e4bf5c5041336
Sowmyashree Shripathi <sowmya4194@gmail.com>
2019-01-01 17:11:37 +0100

    Updated followdots with direction

--- followdot_direction.py
+++ followdot_direction.py
@@ -3,6 +3,8 @@ import sys
 import random
 import time
 from win32api import GetSystemMetrics
+import math
+import numpy as np
 # from copy import deepcopy
 
 timee = time.time()
@@ -19,10 +21,11 @@ screen = pg.display.set_mode(screen_resolution, pg.FULLSCREEN)
 #pg.display.toggle_fullscreen()
 screen_color = (255,255,255)
 dot_color = (255,0,0)
-old_dot_color = (200,200,0)
 line_color = (0,10,10)
 radius = 15
 thickness = 15
+length = 30
+
 try:
     f = open(r'D:\g_truth.txt','w')
 except Exception as e:
@@ -30,6 +33,13 @@ except Exception as e:
 points = [(offset,offset), (max_x,max_y), (max_x, offset), (offset, max_y), (mid_x, mid_y), (offset, mid_y), (mid_x, offset), (mid_x, max_y), (max_x, mid_y)]
 print(points)
 
+
+def arrow_end_point(a,b, c,d):
+#    print ("in function")
+    alpha = length/math.sqrt(math.pow((c-a),2)+math.pow((d-b),2) )
+    x = c +alpha*(a - c)
+    y = d +alpha*(b - d)
+    return int(x),int(y)
 
 # i = 0
 x1, y1 = random.choice(points)
@@ -46,8 +56,11 @@ while (1):
             print("same")
             x,y = random.choice(points)
         try:
-            pg.draw.circle(screen, old_dot_color, (x1, y1), radius, thickness)
-            pg.draw.lines(screen, line_color, False, [(x1,y1), (((x+x1)/2),((y+y1)/2))], 3)
+            pg.draw.circle(screen, dot_color, (x1, y1), radius, thickness)
+            # (((x+x1)/2),((y+y1)/2))
+            (new_x, new_y) = arrow_end_point(x,y,x1,y1)
+            # print("function call ",new_x, new_y, x1, y1, x, y)
+            pg.draw.lines(screen, line_color, False, [(x1,y1), (new_x, new_y)], 3)
             pg.display.update()
             pg.time.delay(500)
             screen.fill(screen_color)
