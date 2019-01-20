76ee5ad963e3efca2d2341f2d71a459303ee0817
Sowmyashree Shripathi <sowmya4194@gmail.com>
2019-01-20 20:15:24 +0100

    Randomly generate positions above threshold

--- followdot_rand.py
+++ followdot_rand.py
@@ -1,20 +1,15 @@
 import pygame as pg
 import sys
-import random
 import time
 from win32api import GetSystemMetrics
 import math
 import numpy as np
+from random import randint
 
 timee = time.time()
 pg.init()
-offset = 10
-# screen_resolution = (GetSystemMetrics(0), GetSystemMetrics(1))
-# mid_x = int(screen_resolution[0]/2)
-# mid_y = int(screen_resolution[1]/2)
-# max_x = screen_resolution[0] - offset
-# max_y = screen_resolution[1] - offset
 
+offset = 10
 res = [GetSystemMetrics(0), GetSystemMetrics(1)]
 screen = pg.display.set_mode(res, pg.FULLSCREEN)
 screen_color = (0,0,0)
@@ -25,12 +20,13 @@ thickness = 15
 length_direction = 30
 arrow_line_thickness = 2
 
+off_val = [offset, offset]   
+end_point = np.subtract(res , off_val)
+
 try:
     f = open(r'D:\g_truth.txt','w')
 except Exception as e:
     print("file exception ",e)
-# points = [(offset,offset), (max_x,max_y), (max_x, offset), (offset, max_y), (mid_x, mid_y), (offset, mid_y), (mid_x, offset), (mid_x, max_y), (max_x, mid_y)]
-# print(points)
 
 
 def arrowline_end_point(a,b, c,d):
@@ -50,32 +46,19 @@ def draw_arrow(screen, colour, start, end):
 
    
 def rand_point_gen():
-    temp_rand1 = [random.uniform(0,1), random.uniform(0,1)]
-    p1 = np.multiply(res,temp_rand1)
-    p1 = p1.astype(int)
-    # temp = (np.add(p1,[radius, radius]))
-    # while ((temp[0] == end_point[0]) or (temp[1] == end_point[1])):
-        # print("equal")
-        # p1 = np.multiply(res,temp_rand1)
-        # p1 = p1.astype(int)
-        # temp = (np.add(p1,[radius, radius]))
+    p1 = np.array([randint(offset, end_point[0]), randint(offset, end_point[1])])
     print(p1)
     return p1
 
    
 def threshold():
-    # max = np.subtract(res, off_val)
     mid = np.divide(res , 2)
     mid = mid.astype(int)
     thresh_dist = np.linalg.norm(mid - off_val)
     return thresh_dist
 
 
-    
-off_val = [offset, offset]   
-end_point = np.subtract(res , off_val)
 thresh = threshold()
-
 p1 = rand_point_gen()
 pg.mouse.set_visible(False)
 while (1):
@@ -88,7 +71,7 @@ while (1):
         screen.fill(screen_color)
         p = rand_point_gen()
         dist = np.linalg.norm(p1 - p)
-        
+                
         while (dist <= thresh):
             print("less than threshold")
             p = rand_point_gen()
